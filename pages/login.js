416d2b37df45757e6f6f107c43de07c688d34e5c
Addsac <leocitton@gmail.com>
2021-12-21 20:40:02 +0100

    edited login page

--- pages/login.js
+++ pages/login.js
@@ -9,7 +9,7 @@ function Login({ providers }) {
         <div key={provider.name}>
           <button
             className="mt-7 bg-green-500 font-medium px-4 py-3 rounded-full hover:opacity-80"
-            onClick={() => signIn(provider.id, { callbackUrl: "/asd" })}
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
           >
             Accedi con {provider.name}
           </button>
